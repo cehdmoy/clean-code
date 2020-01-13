a10056b5c8ade2cb837b51bd93855f728f6119d7
Roberto Moya <robertomoyasuarez@gmail.com>
2020-01-12 22:44:19 -0300

    Update to java 9

--- src/main/java/cl/cehd/cleancode/examples/Iterators.java
+++ src/main/java/cl/cehd/cleancode/examples/Iterators.java
@@ -1,13 +1,14 @@
 package cl.cehd.cleancode.examples;
 
-import com.sun.tools.javac.util.List;
+
+import java.util.List;
 
 public class Iterators {
 
 
     public static void main(String arg[]) {
         List<Integer> list = List.of(1, 8, 11, 7, 9, 21, 22, 2, 5, 10, 12, 400, 10000, 98, 27);
-        
+
 
     }
 
